5d754bba187009750afc91b5a43965347ce5ed6f
LucaPaolella <paolellaluca99@gmail.com>
2023-06-21 13:30:03 +0200

    card

--- src/pages/AppList.vue
+++ src/pages/AppList.vue
@@ -1,26 +1,34 @@
 <script>
+import AppCard from "./AppCard.vue"
 import store from '../store';
 import axios from "axios";
 export default {
-    name: "DoctorList",
+    //name: "DoctorList", //??
+    name: 'AppList',
+    components: {
+        AppCard
+    },
+
     data() {
         return {
             store,
             doctorsList: [],
         }
     },
+
     methods: {
         getDoctors() {
             axios.get(`http://127.0.0.1:8000/api/doctors/${this.$route.params.search}`)
-            .then((response) => {
-                this.doctorsList = response.data.results;
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+                .then((response) => {
+                    this.doctorsList = response.data.results;
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
         }
     },
+
     created() {
         this.getDoctors();
     }
@@ -30,9 +38,16 @@ export default {
 <template>
     <main>
         <div class="container">
-            <ul>
-            <li v-for="doctor in doctorsList">{{ doctor.user.first_name }}</li>
-        </ul>
+
+            <!--<AppCard />-->
+
+            <AppCard v-for="doctor in doctorsList" :key="doctor.id" :doctor="doctor" :address="doctor.address"
+                :phone_number="doctor.phone_number" :services="doctor.services" :user_id="doctor.user_id"
+                :created_at="doctor.created_at" :updated_at="doctor.updated_at" :photo="doctor.photo" />
+
+            <!--<ul>
+                <li v-for="doctor in doctorsList">{{ doctor.user.first_name }}</li>
+            </ul>-->
         </div>
     </main>
 </template>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { reactive } from "vue";
 
 const store = reactive({
-    
+
     apiBaseUrl: import.meta.env.VITE_BACKEND_API_URL,
     // apiDoctorsUrl: apiBaseUrl + "/doctors",
     query: "",
+    testApi: "http://127.0.0.1:8000/storage/",
 })
 
 export default store;
